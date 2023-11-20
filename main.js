0292a09cd95f5e7a4253f6fceb58c0a40ce3216a
LightIce10 <145585156+LightIce10@users.noreply.github.com>
2023-11-20 12:12:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Event Listener
+document.getElementById("1").addEventListener("click", respond1);
+document.getElementById("2").addEventListener("click", respond2);
+document.getElementById("3").addEventListener("click", respond3);
+document.getElementById("4").addEventListener("click", respond4);
+document.getElementById("5").addEventListener("click", respond5);
+
+function respond1() {
+  alert("disguise");
+}
+
+function respond2() {
+  alert("cats and dogs");
+}
+function respond3() {
+  alert("leg");
+}
+
+function respond4() {
+  alert("weather");
+}
+
+function respond5() {
+  alert("worlds");
+}
